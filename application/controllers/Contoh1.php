eae8c565d214d093a63778c5e2aa8eb6a8367069
IlhamKamal-im <ilhamkamal39@gmail.com>
2023-03-23 10:20:55 +0700

    Commit

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
     public function index() 
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama Saya Ilham Kamal Saya Tinggal di Depok Olahraga yang Saya Suka Adalah TepokBulu";   
+        echo "Nama Saya Ilham Kamal Saya Tinggal di Depok Olahraga yang Saya Suka Adalah BuluTangkis";   
     }
 }
