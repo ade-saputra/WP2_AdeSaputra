dab71e61fa945a4fb001a04e79ee7070c467cc35
IlhamKamal-im <ilhamkamal39@gmail.com>
2023-04-02 15:25:05 +0700

    Mengerjakan contoh1,contoh2,contoh3 dan menghilangkan index.php

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -7,10 +7,21 @@ class Latihan1 extends CI_Controller
         //$this->load-view('view-latihan1');
     }
 
-    public function penjumlahan($n1, $n2)
+    public function penjumlahan($n1, $n2) 
     {
         $this->load->model('Model_latihan1');
-        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = " .$hasil;
+
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+
+        $this->load->view('view-latihan', $data);
     }
+
+    // public function penjumlahan($n1, $n2)
+    // {
+    //     $this->load->model('Model_latihan1');
+    //     $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+    //     echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = " .$hasil;
+    // }
 }
--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -6,6 +6,6 @@
         Halo Kawan.. Yuk kita belajar web programming..!!!<br>
         Nilai 1 = <?= $nilai1; ?>
         Nilai 2 = <?= $nilai2; ?>
-        ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $niali1 . " + " . $nilai2 . " = " . $hasil; ?>
+        ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
     </body>
 </html>
