94f6087b9c6060a7e0bb062889eb4744beb36728
IlhamKamal-im <ilhamkamal39@gmail.com>
2023-04-06 10:12:15 +0700

    Mengerjakan tugas pertemuan 3&4

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,9 +1,12 @@
 <?php
-class Contoh1 extends CI_Controller 
+class Contoh1 extends CI_Controller
 {
-    public function index() 
-    {
-        echo "<h1>Perkenalkan</h1>";
-        echo "Nama Saya Ilham Kamal Saya Tinggal di Depok Olahraga yang Saya Suka Adalah BuluTangkis";   
-    }
+  public function index()
+ {
+   echo "<h1>Perkenalkan</h1>";
+   echo"Nama saya Angga Syahputra
+   Saya tingga di daerah Bogor
+   olahraga yang saya sukai adalah
+   Basket";
+ }
 }
